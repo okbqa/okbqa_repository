8145c9d29022b95f39e58e5e693c702e53be51cc
Jin-Dong Kim <jindong.kim@gmail.com>
2016-01-13 16:59:50 +0900

    REVISED category specification is linked to the corresponding documantation.

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,2 +1,8 @@
 module ComponentsHelper
+
+	def link_to_category(category)
+		label = category.gsub(' ', '').downcase
+		"<a href='http://35.okbqa.org/development/architecture/#{label}'>#{category}</a> "
+	end
+
 end
