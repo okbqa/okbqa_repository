d8d2bb892a09856ca264f732ae3f2d4b6bcb7198
Jin-Dong Kim <jindong.kim@gmail.com>
2016-03-05 15:54:19 +0900

    REVISED category documentation moved to www.okbqa.org.

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,11 +1,11 @@
 module ComponentsHelper
 
 	def link_to_category(category)
-		label = category.gsub(' ', '').downcase
+		label = category.gsub(' ', '-').downcase
 		if label == 'other'
 			category
 		else
-			"<a href='http://35.okbqa.org/development/architecture/#{label}'>#{category}</a> "
+			"<a href='http://www.okbqa.org/architecture/#{label}'>#{category}</a> "
 		end
 	end
 
