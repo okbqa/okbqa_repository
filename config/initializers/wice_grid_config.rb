3fd0addfdf8946fb5ff71ce9171fa937e1eca867
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2016-11-26 11:45:09 +0900

    REVISED style

--- config/initializers/wice_grid_config.rb
+++ config/initializers/wice_grid_config.rb
@@ -1,7 +1,7 @@
 if defined?(Wice::Defaults)
 
   # Default number of rows to show per page.
-  Wice::Defaults::PER_PAGE = 20
+  Wice::Defaults::PER_PAGE = 5
 
   # Default order direction
   Wice::Defaults::ORDER_DIRECTION = 'asc'
